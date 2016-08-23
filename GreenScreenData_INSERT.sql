ec349c3806bdcb9972fb2a091f9bed06903918d9
Schabernack <felix.schabernack1@hp.com>
2016-08-23 12:43:42 -0700

    More INSERT files

---
+++ GreenScreenData_INSERT.sql
@@ -0,0 +1,60 @@
+
+INSERT INTO GreenScreenData (status_name, status_description, status_category)
+VALUES('carcinogenicity','Carcinogenicity Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('mutagenicity_genotoxicity','Mutagenicity & Genotoxicity Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('reproductive_toxicity','Reproductive Toxicity Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('developmental_toxicity','Developmental Toxicity Notes', ' To be decided'), 
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('endocrine_activity','Endocrine Activity Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('acute_mammalian_toxicity','Acute Mammalian Toxicity Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('systemic_toxicity_organ_effects_single','Systemic Toxicity & Organ Effects - Single Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('systemic_toxicity_organ_effects_repeat','Systemic Toxicity & Organ Effects - Repeat Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('neurotoxicity_single','Neurotoxicity - Single Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('neurotoxicity_repeat','Neurotoxicity - Repeat Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('sensitization','Sensitization Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('respiratory_sensitization','Respiratory Sensitization Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('skin_irritation','Skin Irritation Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('eye_irritation','Eye Irritation Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('acute_aquatic_toxicity','Acute Aquatic Toxicity Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('chronic_aquatic_toxicity','Chronic Aquatic Toxicity Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('persistence','Persistence Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('bioaccumulation','Bioaccumulation Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('reactivity,Reactivity Notes', ' To be decided');
+
+INSERT INTO GreenScreenData(status_name, status_description, status_category)
+VALUES('flammability','Flammability Notes');
---
+++ RegulatedMetal_INSERT.sql
@@ -0,0 +1,53 @@
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES ('as','Arsenic (As)', 5, 5, 500, 2);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('ba', 'Barium (Ba)', 100, 100, 10000, 2);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('be', 'Beryllium (Be)', 0, 0.75, 75);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('cd', 'Cadmium (Cd)', 1, 1, 100, 2);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('co', 'Cobalt (Co)', 0, 80, 8000, 2);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('cr', 'Total Chrome', 5, 5, 500,	2);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('cr3', 'Chromium III (Cr III)', 5, 5, 2500, 0);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('cr6', 'Chromium VI (Cr VI)', 5, 5, 500, 0)
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('cu', 'Copper (Cu)', 0 , 25, 2500, 0");
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('hg', 'Mercury (Hg)', 0.2, 0.2, 20, 2);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('mo', 'Molybdenum (Mo)', 0, 350, 3500, 0);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)    
+VALUES('ni', 'Nickel (Ni)', 0, 20, 2000, 2);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('pb', 'Lead (Pb)', 5, 5, 1000, 2);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('sb', 'Antimony (Sb)', 0, 15, 500, 2);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('se', 'Selenium (Se)', 1, 1, 100, 2);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('ti', 'Thallium (Tl)', 0, 7, 700, 0);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('v', 'Vanadium (V)', 0,25, 2400, 0);
+
+INSERT INTO RegulatedMetal(criteria_name, criteria_description, us_tlcp, ca_stlc, ca_ttlc, dfe_limit)
+VALUES('zn', 'Zinc (Zn)', 0, 250, 5000, 0);
